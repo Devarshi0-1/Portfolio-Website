7210d3d86d6b0f01a95c21dab0c01b0d900d80e4
Devarshi <devarshidwi@gmail.com>
2023-01-16 01:31:10 +0530

    Added New Fav Icon

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,6 +1,6 @@
 import './About.css'
 import aboutImg from '../../../public/assets/portfolioImgs/aboutImg.webp'
-import CV from '../../../public/assets/My_Resume.pdf'
+import CV from '../../../public/assets/Devarshi Resume.pdf'
 
 function About() {
     return (
