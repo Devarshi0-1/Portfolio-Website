247400d6404c53685cc06d9e915b80ec9418ca3f
Devarshi <devarshidwi@gmail.com>
2023-01-16 21:37:56 +0530

    Added better/responsive styling for contact section and form validation for the same

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,20 +1,30 @@
 import { useRef } from 'react'
 import emailjs from 'emailjs-com'
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 import './contact.css'
+
+const schema = yup.object().shape({
+    name: yup.string("Should Be A String").required("Name is Required"),
+    email: yup.string("Should Be A String").email("Enter an Valid Email Address").required("Email is Required"),
+    subject: yup.string("Should Be A String").required("Subject is Required"),
+    message: yup.string("Should Be A String").required("Message is Required")
+})
 
 function Contact() {
     const form = useRef();
+    const { register, handleSubmit, formState:{errors} } = useForm({
+        resolver: yupResolver(schema)
+    })
 
-    const sendEmail = (e) => {
-        e.preventDefault();
-
+    const sendEmail = () => {
         emailjs.sendForm('service_5jy0pyd', 'template_84825uq', form.current, 'TlCkPF1bWfjNQoRfp')
             .then((result) => {
                 console.log(`Email processed with Status: ${result.text}`);
             }, (error) => {
                 console.log(error.text);
             });
-        e.target.reset()
     };
 
     return (
@@ -24,11 +34,15 @@ function Contact() {
             <div className="contactContainer">
                 <div className="formCont">
                     <h2>Let's Build Something Great Together</h2>
-                    <form ref={form} onSubmit={sendEmail}>
-                        <input type="text" placeholder='Name' name='name' />
-                        <input type="email" placeholder='Your Email' name='email' />
-                        <input type="text" placeholder='Project' name='subject' />
-                        <textarea placeholder='Message' name='message'></textarea>
+                    <form ref={form} onSubmit={handleSubmit(sendEmail)}>
+                        <input {...register('name')} type="text" placeholder='Name' name='name' />
+                        <p>{errors.name?.message}</p>
+                        <input {...register('email')} type="email" placeholder='Your Email' name='email' />
+                        <p>{errors.email?.message}</p>
+                        <input {...register('subject')} type="text" placeholder='Project' name='subject' />
+                        <p>{errors.subject?.message}</p>
+                        <textarea {...register('message')} placeholder='Message' name='message'></textarea>
+                        <p>{errors.message?.message}</p>
                         <button className='primaryBtn'>Send Message</button>
                     </form>
                 </div>
