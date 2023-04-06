335a847254ff19dc840b99a79e51cc814ad9681c
Devarshi <devarshidwi@gmail.com>
2023-04-06 13:08:28 +0530

    Adding Firebase config file to project

---
+++ src/firebaseBackend.jsx
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD5EsTJpdvpBQl1JL6KEDaGLDd8LgG5fvM",
+    authDomain: "personal-portfolio-backe-9e773.firebaseapp.com",
+    projectId: "personal-portfolio-backe-9e773",
+    storageBucket: "personal-portfolio-backe-9e773.appspot.com",
+    messagingSenderId: "901046555015",
+    appId: "1:901046555015:web:4a869f526944bb70f40322",
+}
+
+const app = initializeApp(firebaseConfig)
