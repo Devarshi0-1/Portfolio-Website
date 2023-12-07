b8cd5ec26bc9a32ad13d3fb1d378f38d8e795b36
Devarshi <devarshidwi@gmail.com>
2023-12-07 05:32:43 +0530

    Revamped the whole website and done with desktop version

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,64 +1,56 @@
-import { AiFillEye } from "react-icons/ai"
-import CV from "../../assets/Devarshi Resume.pdf"
+import CV from "../../assets/Devarshi_Resume.pdf"
 import "./About.css"
 
 function About() {
     return (
         <section id='about' className='flex-center'>
             <h1>About</h1>
-            <p>
-                In <span>2020</span>, I finished my <span>Schooling</span> and
-                pursued <span>Computer Science Engineering</span>. I immersed
-                myself in the dynamic realm of <span>Frontend Development</span>
-                . Since then, I've been creating both 
-                <span> Personal</span> and
-                <span> Professional</span> projects, delivering high-quality and
-                aesthetically pleasing code with passion. Consequently, I've
-                successfully transitioned into a{" "}
-                <span>Full-stack developer</span>
-            </p>
+            <div className='aboutWrapper'>
+                <div className='aboutMe'>
+                    <p>
+                        In <span>2020</span>, I finished my{" "}
+                        <span>Schooling</span> and pursued{" "}
+                        <span>Computer Science Engineering</span>. I immersed
+                        myself in the dynamic realm of{" "}
+                        <span>Frontend Development</span>. Since then, I've been
+                        creating both
+                        <span> Personal</span> and
+                        <span> Professional</span> projects, delivering
+                        high-quality and aesthetically pleasing code with
+                        passion. Consequently, I've successfully transitioned
+                        into a <span>Full-stack developer</span>
+                    </p>
+                </div>
+                <div className='experience'>
+                    <div className='yearsExp'>
+                        <p>02+</p>
+                        <p>
+                            Years
+                            <br />
+                            Experience
+                        </p>
+                    </div>
+                    <div className='projects'>
+                        <p>10+</p>
+                        <p>
+                            Completed
+                            <br />
+                            Projects
+                        </p>
+                    </div>
+                    <div className='workExp'>
+                        <p>2</p>
+                        <p>Internships</p>
+                    </div>
+                </div>
+            </div>
+            <button className='primaryBtn'>
+                <a className='flex-center' href={CV} target='_blank'>
+                    View Resume
+                </a>
+            </button>
         </section>
     )
 }
 
 export default About
-
-{
-    /* <h1>About Me</h1>
-<h2>My Introduction</h2>
-<div className='aboutGridContainer'>
-    <img className='aboutImg' src={aboutImg} loading='lazy' />
-    <p>
-        Web developer, with extensive knowledge and years of
-        experience, working in web technologies and UI/UX,
-        delivering quality work.
-    </p>
-    <div className='experienceSection'>
-        <div className='yearsExp'>
-            <p>02+</p>
-            <p>
-                Years
-                <br />
-                Experience
-            </p>
-        </div>
-        <div className='projects'>
-            <p>10+</p>
-            <p>
-                Completed
-                <br />
-                Projects
-            </p>
-        </div>
-        <div className='workExp'>
-            <p>2</p>
-            <p>Internships</p>
-        </div>
-        <button className='primaryBtn'>
-            <a className='flex-center' href={CV} target='_blank'>
-                View Resume <AiFillEye />
-            </a>
-        </button>
-    </div>
-</div> */
-}
